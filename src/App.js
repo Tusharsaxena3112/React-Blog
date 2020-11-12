5d8c95d5a8547629998e5ac64edfb4e4629f94db
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-11-12 16:49:47 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-import Home from './Screens/Home';
+import Home from "./Screens/Home";
+import Category from "./Screens/Category";
+import Single from "./Screens/Single";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const App = () =>{
-	return <>
-		<Home/>
-	</>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/">
+        <Home />
+      </Route>
+	  <Route path="/category">
+        <Category />
+      </Route>
+	  <Route path="/single">
+        <Single />
+      </Route>
+    </BrowserRouter>
+  );
 };
 
 export default App;
