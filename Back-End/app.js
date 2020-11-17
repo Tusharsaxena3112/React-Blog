673abfddd9c88047e5a65da48d2ef1dd88235a69
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-11-17 10:26:04 +0530

    connecting mongodb

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const mongoose  = require('mongoose');
+const {MONGOURI} = require('./key');
 const app = express();
 const PORT = 5000;
 
+//Connection to MONGODB
+mongoose.connect(MONGOURI);
+mongoose.connection.on('connected',()=>{
+    console.log('connected');
+});
+mongoose.connection.on('error',(err)=>{
+    console.log(err);
+});
+
+
+app.listen(PORT,()=>{
+    console.log('Server is running at 5000');
+});
---
+++ Back-End/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI:'mongodb+srv://Tushar-Saxena:React-Blog@cluster0.3sspr.mongodb.net/test?retryWrites=true&w=majority'
+}
